96c7bf206e2eb62863e6e85f44596a4b97f8fb47
snatu1996 <shubham.natu@cognizant.com>
2020-12-01 13:23:16 +0530

    Toggling colors using useState

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import {useState} from 'react';
 
 function App() {
   const [count,setCount] = useState(0); //useState() returns an updater function along with set function, which has access to previous state value
+  const [toggle,setToggel] = useState(false);
 
   const incremetCount = () => {
     setCount(prevCount => prevCount+1);
   }
 
+  const toggleChange = () => {
+    setToggel(prevToggel => !prevToggel);
+  }
+
   return (
+    <>
     <div className="App">
       <button onClick={incremetCount}>Button clicked {count} times</button>
     </div>
+    <div onClick={toggleChange} style={{height: "50px", width: "50px", background: toggle? "yellow" : "grey"}}/>
+    </>
   );
 }
 
