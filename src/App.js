8f403adfd018d24d6bc061464363feee822d295d
snatu1996 <shubham.natu@cognizant.com>
2020-12-02 15:37:59 +0530

    Using useEffect to fetch location

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import {useState, useEffect} from 'react';  //useEffect is used to handle side effects or for interacting with outer things like API
 
+const initialLocation = {
+  latitude: null,
+  longitude: null,
+  speed: null
+}      //Declaring initialState outside of functional component
+
 function App() {
   const [count,setCount] = useState(0); //useState() returns an updater function along with set function, which has access to previous state value
   const [toggle,setToggel] = useState(false);
   const [mousePosition, setMousePosition] = useState({x: null, y: null}); //Using useState to add an object to state
   const [status, setStatus] = useState(navigator.onLine);
+  const [location,setLocation] = useState(initialLocation);   //Can also destructure in place
+  
+  let mounted = true;
 
   useEffect(() => {
     document.title = `Clicked - ${count} times`;
     window.addEventListener("mousemove", handleMouseMove);
     window.addEventListener("online", handleOnline);
     window.addEventListener("offline", handleOffline);
+    navigator.geolocation.getCurrentPosition(handleLocation);
+    const watchId = navigator.geolocation.watchPosition(handleLocation);
 
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
       window.removeEventListener("online", handleOnline);
       window.removeEventListener("offline", handleOffline);
+      navigator.geolocation.clearWatch(watchId);
+      mounted = false;
     }
   }, [count])   //useEffect gets called everytime after render executes.
 
   // To make useEffect behave like componentWillUnmount, we return a funtion from it, this function is returned everytime before a render, to cleanup effects of previous render and when component unmounts
   //To prevent useEffect being called after and before every render, we can pass a second argument, which is an array of dynamic static values which are changing, if we keep it empty, then impact of state change will not be visible as when we return from useEffect previous render changes are flushed.
 
+  const handleLocation = (event) => {
+    if(mounted) {
+      setLocation({
+        latitude: event.latitude,
+        longitude: event.longitude,
+        speed: event.speed
+      })
+    }
+  }
+  
   const incremetCount = () => {
     setCount(prevCount => prevCount+1);
   }
@@ -57,6 +80,8 @@ function App() {
     <h3>{`x - ${mousePosition.x}`}</h3>
     <h3>{`y - ${mousePosition.y}`}</h3>
     <h2>{`Network Status!!! ${status}`}</h2>
+    <h2>Location!!!!</h2>
+    <h3>{JSON.stringify(location)}</h3>
     </>
   );
 }
