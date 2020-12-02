daefd5276e8096dae941e22a5f388d11d6107734
snatu1996 <shubham.natu@cognizant.com>
2020-12-02 12:55:42 +0530

    Basic use of useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';  //useEffect is used to handle side effects or for interacting with outer things like API
 
 function App() {
   const [count,setCount] = useState(0); //useState() returns an updater function along with set function, which has access to previous state value
   const [toggle,setToggel] = useState(false);
+
+  useEffect(() => {
+    document.title = `Clicked - ${count} times`;
+  })   //useEffect gets called everytime after render executes.
 
   const incremetCount = () => {
     setCount(prevCount => prevCount+1);
